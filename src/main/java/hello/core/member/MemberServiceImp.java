48c63100e5b2b4972450c3e7d9010e440600431c
Parkyuchan <dbcks4848@naver.com>
2025-03-09 20:28:28 +0900

    스프링 변경 시작

---
+++ src/main/java/hello/core/member/MemberServiceImp.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+public class MemberServiceImp implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImp(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
